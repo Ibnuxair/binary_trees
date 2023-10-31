39f70f483851f3360357ed9d5f9aebf4975a017f
Ibnuxair <ibrahimuzairu28@gmail.com>
2023-10-31 03:13:26 -0700

    Header

--- binary_trees.h
+++ binary_trees.h
@@ -1,31 +1,7 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-
-/* This is the header files */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stddef.h>
-
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
-typedef struct binary_tree_s binary_tree_t;
-
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -37,17 +13,19 @@ typedef struct binary_tree_s binary_tree_t;
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
+/* Type aliases for different types of binary trees */
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
-
-/* This is the function prototypes */
+/* Functions Prototypes */
 void binary_tree_print(const binary_tree_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* ENDIF */
